cdbca429fa77fce4f055c422ebdab86bbc458577
Qossay ZeinEddin <103140839+QossayZeineddin@users.noreply.github.com>
2023-05-29 13:57:57 +0300

    fix test1

--- app/core/models.py
+++ app/core/models.py
@@ -8,15 +8,15 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, Permi
 
 
 class UserManager(BaseUserManager):
-    def create_user(self , email , password=None  , **extra_field):
+    def create_user(self, email, password=None, **extra_field):
         if not email:
             raise ValueError('User must have an email address.')
-        user = self.model(email = self.normalize_email(email) ,  **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self , email , password=None  , **extra_field):
+    def create_superuser(self, email, password=None, **extra_field):
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_field)
@@ -26,16 +26,17 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser , PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin):
     """users in the project"""
-    email = models.EmailField(blank = False ,max_length=255 , unique=True)
-    name = models.CharField(blank = False,max_length=200)
+    email = models.EmailField(blank=False, max_length=255, unique=True)
+    name = models.CharField(blank=False, max_length=200)
     joining_date = models.DateTimeField(auto_now_add=True)
     birthday = models.DateField(default='2000-10-18', verbose_name='Date of Birth', help_text='Enter your birthdate')
-    major = models.CharField(blank=True , max_length=100)
+    major = models.CharField(blank=True, max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD  = 'email'
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ testing the models in the project
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-#from django.core.management.base import BaseCommand
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successfully(self):
@@ -15,12 +15,12 @@ class ModelTests(TestCase):
 
         try:
             user = get_user_model().objects.create_user(
-                email = email,
-                password = password,
-                major = major,
-                birthday = birthday,
+                email=email,
+                password=password,
+                major=major,
+                birthday=birthday,
             )
-            self.assertEquals(user.email , email)
+            self.assertEquals(user.email, email)
             self.assertTrue(user.check_password(password))
 
         except:
@@ -38,7 +38,7 @@ class ModelTests(TestCase):
         birthday = '2000-10-18'
         password = "test123"
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email,password = password,major = major,birthday = birthday,)
+            user = get_user_model().objects.create_user(email, password=password, major=major, birthday=birthday)
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
@@ -47,7 +47,7 @@ class ModelTests(TestCase):
         birthday = '2000-10-18'
         password = "test123"
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', password = password,major = major,birthday = birthday,)
+            get_user_model().objects.create_user('', password=password, major=major, birthday=birthday, )
 
     def test_create_superuser(self):
         """Test creating a superuser."""
@@ -55,6 +55,6 @@ class ModelTests(TestCase):
         major = "test"
         birthday = '2000-10-18'
         password = "test123"
-        user = get_user_model().objects.create_superuser(email, password = password,major = major,birthday = birthday)
+        user = get_user_model().objects.create_superuser(email, password=password, major=major, birthday=birthday)
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
