e21a7c7478c0299f8041b7746147a53a9e4c0500
Qossay ZeinEddin <103140839+QossayZeineddin@users.noreply.github.com>
2023-06-04 09:15:04 +0300

    create  user app and  api (get , update, put , patch, delete)

---
+++ app/user/views.py
@@ -0,0 +1,29 @@
+"""
+Views for the user API.
+"""
+from rest_framework import generics, authentication, permissions, status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from .serializers import (UserSerializer, AuthTokenSerializer)
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in the system by api reqoust // this method handle the post request"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    """to manage data that come from authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
