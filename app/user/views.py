99edb2334d1482cbd55ed8d3820a0a2e0dbb7ed7
Qossay ZeinEddin <103140839+QossayZeineddin@users.noreply.github.com>
2023-06-06 17:11:11 +0300

    return user info when this user carate this token and be authteted

--- app/user/views.py
+++ app/user/views.py
@@ -5,17 +5,32 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from .serializers import (UserSerializer, AuthTokenSerializer)
+from rest_framework.response import Response
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create new user in the system by api reqoust // this method handle the post request"""
     serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        return Response({
+            'user_id': user.id,
+            'email': user.email,
+            'birthday': user.birthday,
+            'token': user.auth_token.key,
+        })
 
 
 class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
