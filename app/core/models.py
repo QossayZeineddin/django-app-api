1c11bdf7bae56e175e94eb9b3a2355d4d6426c61
Qossay ZeinEddin <103140839+QossayZeineddin@users.noreply.github.com>
2023-06-07 11:55:31 +0300

    add auth token Expiration time v2 fix test

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,9 @@
 create the database models
 
 """
-from datetime import timedelta
-from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from django.conf import settings
-from rest_framework.authtoken.models import Token
 
 
 class UserManager(BaseUserManager):
